95b77a5f5e852e043eeaea30a7c0ec53684a93b6
Isaac Vieira <isaacdsvieira2008@gmail.com>
2026-02-08 22:21:20 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,37 @@
+let inputTexto = document.querySelector(".input-texto");
+let traducao = document.querySelector(".traducao");
+let idioma = document.querySelector(".idioma");
+
+async function traduzir(){
+
+    let endereco = "https://api.mymemory.translated.net/get?q="
+        + inputTexto.value
+        + "&langpair=pt-BR|"
+        + idioma.value
+
+    let resposta = await fetch(endereco)
+
+    let dados = await resposta.json()
+
+    traducao.innerHTML = dados.responseData.translatedText
+
+    console.log(dados)
+}
+
+function ouvirVoz () {
+    let voz = window.webkitSpeechRecognition
+
+    let reconhecimentoVoz = new voz()
+
+    reconhecimentoVoz.lang = "pt-BR"
+
+    reconhecimentoVoz.onresult = (evento) => {
+        let textoTranscricao = evento.results[0][0].transcript
+
+        inputTexto.textContent = textoTranscricao
+
+        traduzir()
+    }
+
+    reconhecimentoVoz.start () 
+}
